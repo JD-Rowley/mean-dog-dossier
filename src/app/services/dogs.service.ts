f83e522daaba70203cf441e7c91e1e7c8b952ac0
JD Rowley <jdrowley1989@gmail.com>
2022-10-12 18:10:56 -0600

    dog cards on the browse page have a uniformity

--- src/app/services/dogs.service.ts
+++ src/app/services/dogs.service.ts
@@ -4,22 +4,10 @@ import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class DogsService {
-  // dogs = [];
   apiKey = environment.API_KEY;
 
   constructor(private http: HttpClient) { }
 
-  // async getRandomDogs() {
-  //   const res = await fetch('https://api.thedogapi.com/v1/images/search?mime_types=gif&limit=3', {
-  //     method: 'GET',
-  //     headers: ({
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json',
-  //       'x-api-key': this.apiKey
-  //     })
-  //   });
-  //   console.log(res);
-  // }
   getDogGifs() {
     return this.http.get('https://api.thedogapi.com/v1/images/search?mime_types=gif&limit=3');
   }
